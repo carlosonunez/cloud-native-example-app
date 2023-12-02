b9f9be3fc4e6e7d95df58b850a8e807b1732d565
Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>
2023-12-01 18:43:52 -0600

    [infra] make this a little bigger so services fit
    Signed-off-by: Carlos Nunez <13461447+carlosonunez@users.noreply.github.com>

--- infra/eks.tf
+++ infra/eks.tf
@@ -31,7 +31,7 @@ module "eks" {
   subnet_ids               = module.vpc.private_subnets
   control_plane_subnet_ids = module.vpc.public_subnets
   eks_managed_node_group_defaults = {
-    instance_types = ["t3g.medium"]
+    instance_types = ["t4g.large"]
     capacity_type  = "SPOT"
     desired_size   = 1
     min_size       = 1
